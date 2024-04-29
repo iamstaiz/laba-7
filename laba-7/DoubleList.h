8028998f2cdcf6e4e4af8d36425a4032f6835455
Nastia <kasianenko.anastasiia@chnu.edu.ua>
2024-04-30 00:49:29 +0300

    Create class for DoubleList

--- laba-7/DoubleList.h
+++ laba-7/DoubleList.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+using namespace std;
+#include "memory"
+
+template<typename T>
+struct Node2 {
+    T data;
+    shared_ptr<Node2<T>> next;
+    shared_ptr<Node2<T>> prev;
+};
+
+template<typename T>
+class DoubleList {
+private:
+    shared_ptr<Node2<T>> head;
+    shared_ptr<Node2<T>> tail;
+    size_t size;
+
+    shared_ptr<Node2<T>> getNode(size_t index) const;
+};
