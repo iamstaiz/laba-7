0239e0895c621144c63b3b2d82027f53d24ccc10
Nastia <kasianenko.anastasiia@chnu.edu.ua>
2024-04-30 00:33:48 +0300

    Add struct Node. create class SingleList, add 2 methods Push

--- laba-7/SingleList.h
+++ laba-7/SingleList.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <iostream>
+using namespace std;
+#include "memory"
+
+template<typename T>
+struct Node {
+    T data;
+    shared_ptr<Node<T>> next;
+
+    Node(const T& value) : data(value), next(nullptr) {}
+
+};
+
+template<typename T>
+class SingleList {
+private:
+    shared_ptr<Node<T>> head;
+    size_t size;
+public:
+
+    SingleList() :head(nullptr), size(0) {}
+
+    void pushFront(const T& value);
+    void pushBack(const T& value);
+
+
+};
