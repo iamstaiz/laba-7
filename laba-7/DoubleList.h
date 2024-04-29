f3a468838ee2f64e54f19e655ec75286d06b465d
Nastia <kasianenko.anastasiia@chnu.edu.ua>
2024-04-30 00:56:40 +0300

    Add two methods Push_back Push_Front for DoubleList

--- laba-7/DoubleList.h
+++ laba-7/DoubleList.h
@@ -18,4 +18,43 @@ private:
     size_t size;
 
     shared_ptr<Node2<T>> getNode(size_t index) const;
+
+public:
+    DoubleList() : head(nullptr), tail(nullptr), size(0) {}
+
+    void pushBack(const T& value);
+    void pushFront(const T& value);
 };
+
+template<typename T>
+void DoubleList<T>::pushBack(const T& value) {
+    unique_ptr<Node2<T>> newNode = make_unique<Node2<T>>();
+    newNode->data = value;
+    newNode->next = nullptr;
+    newNode->prev = tail;
+    if (tail != nullptr) {
+        tail->next = move(newNode);
+        tail = tail->next;
+    }
+    else {
+        head = move(newNode);
+        tail = head;
+    }
+    size++;
+}
+
+template<typename T>
+void DoubleList<T>::pushFront(const T& value) {
+    unique_ptr<Node2<T>> newNode = make_unique<Node2<T>>();
+    newNode->data = value;
+    newNode->next = head;
+    newNode->prev = nullptr;
+    if (head != nullptr) {
+        head->prev = move(newNode);
+    }
+    else {
+        tail = move(newNode);
+    }
+    head = newNode->prev;
+    size++;
+}
